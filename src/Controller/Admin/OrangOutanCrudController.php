95a43f522072c85e5eaa0566a9e652b3b4d4e157
plbonnier <pl_bonnier@yahoo.fr>
2024-02-01 23:59:09 +0100

    start debug

--- src/Controller/Admin/OrangOutanCrudController.php
+++ src/Controller/Admin/OrangOutanCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
 class OrangOutanCrudController extends AbstractCrudController
 {
@@ -15,14 +17,21 @@ class OrangOutanCrudController extends AbstractCrudController
         return OrangOutan::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name')
+            ->setLabel('Nom'),
+            DateTimeField::new('birth')
+            ->setFormat('dd/MM/yyyy')
+            ->setLabel('Date de naissance'),
+            TextEditorField::new('story')
+            ->setLabel('Leur histoire'),
+            ImageField::new('picture')
+            ->setBasePath('uploads/images/pictures/')
+            ->setUploadDir('public/uploads/images/pictures/')
+            ->setLabel('Photo'),
         ];
     }
-    */
 }
