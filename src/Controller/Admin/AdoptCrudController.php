20d2ad884ab54c26a28901ca879ad618215759e8
plbonnier <pl_bonnier@yahoo.fr>
2024-02-02 11:43:34 +0100

    debug ok

--- src/Controller/Admin/AdoptCrudController.php
+++ src/Controller/Admin/AdoptCrudController.php
@@ -5,8 +5,9 @@ namespace App\Controller\Admin;
 use App\Entity\Adopt;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class AdoptCrudController extends AbstractCrudController
 {
@@ -15,14 +16,24 @@ class AdoptCrudController extends AbstractCrudController
         return Adopt::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            MoneyField::new('money')->setLabel('Donation')->setCurrency('EUR'),
+            DateField::new('date')->setLabel('Date du parrainage'),
+            AssociationField::new('user')
+            ->autocomplete()
+            ->formatValue(function ($value, $entity) {
+                // Utiliser $value->getLastname() si 'user' est une entité User
+                return $value->getLastName();
+            }),
+        AssociationField::new('orangoutan')
+            ->autocomplete()
+            ->formatValue(function ($value, $entity) {
+                // Utiliser $value->getName() si 'orangoutan' est une entité OrangOutan
+                return $value->getName();
+            }),
         ];
     }
-    */
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FileField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,18 +19,24 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('email'),
+            ArrayField::new('roles'),
+            TextField::new('password')->hideOnIndex(),
+            TextField::new('lastname')->setLabel('Nom de famille'),
+            TextField::new('firstname')->setLabel('Prénom'),
+            TextField::new('adress')->setLabel('Adresse'),
+            TextField::new('zipcode')->setLabel('Code postal'),
+            TextField::new('city')->setLabel('Ville'),
+            TextField::new('country')->setLabel('Pays'),
+            BooleanField::new('rgpd'),
             ImageField::new('picture')
-            ->setBasePath('uploads/images/pictures/')
-            ->setUploadDir('public/uploads/images/pictures/')
-            ->setLabel('Photo'),
+                ->setBasePath('uploads/images/pictures/')
+                ->setUploadDir('public/uploads/images/pictures/')
+                ->setLabel('Photo')
         ];
     }
-    */
 }
